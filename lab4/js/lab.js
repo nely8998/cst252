bd19de03c1c3d3197e243ec6c61caecd25f73e27
Nely1226 <nelyflores116@gmail.com>
2021-02-09 18:22:16 -0800

    add to github

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// How to Manage your time.js
+// A how to manage your time task
+//
+/// Author: Nely Flores <nflores-ramirez@csumb.edu>
+// Date: 2021
+//
+
+// Write down all your tasks
+  // Make a list of tasks
+    // Create a planner
+      // Set reminders
+    // Mark off your tasks along the way
+    // Repeat
