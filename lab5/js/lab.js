902004d4f9f10acdb612f7ed7a226b13f3c152bc
Nely1226 <nelyflores116@gmail.com>
2021-02-13 15:50:59 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,18 @@
+/*
+ Author: Nely Flores <nflores-ramirez@csumb.edu>
+ Created: 11 Feburary
+ License: Public Domain
+*/
+
+
+// Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004
+ make = "Toyota";
+ model = "highlander";
+ color = "Blue";
+ year = 2016;
+// Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = true;
+// Create a new variable age that subtracts the year from the current year
+var age = new Date (2021).getFullYear(2017);
+// Use console.log() to print the value of age, example, console.log("Age: " + age)
+ console.log("Age: " + age + "<br>");
