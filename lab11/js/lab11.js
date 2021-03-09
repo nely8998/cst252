4e5c2ba9cfb10402b3f70ed3a72714744815b6d4
Nely1226 <nelyflores116@gmail.com>
2021-03-08 17:04:41 -0800

    finished lab 11

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -3,3 +3,31 @@
  Created: 10 March
  License: Public Domain
 */
+
+// depending 4
+function sortingHat(str) {
+len = str.length;
+mod = len % 4;
+if (mod == 0) {
+  return "Gryffindor"
+}
+else if (mod == 1) {
+  return "Ravenclaw"
+}
+else if (mod === 2) {
+  return "Slytherin"
+}
+else if (mod == 3) {
+  return "Hufflepuff"
+}
+}
+
+// add a button
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  // grt value from input field
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The sorting Hat has sorted you into" + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+})
