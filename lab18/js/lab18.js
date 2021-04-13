809b645b4f2c0d3f021a4ffecb9b8e9212cbc7a5
Nely1226 <nelyflores116@gmail.com>
2021-04-12 19:24:30 -0700

    new lab

---
+++ lab18/js/lab18.js
@@ -0,0 +1,20 @@
+/*
+ Author: Nely Flores <nflores-ramirez@csumb.edu>
+ Created: 7 April
+ License: Public Domain
+*/
+
+// add event listner to button-one
+$("#button-one").click(function(){
+$("#button-one").toggleClass("first")
+})
+
+// add event listner to button-two
+$("#button-two").click(function(){
+$("#button-two").toggleClass("second")
+})
+
+// add event listner to button-three
+$("#button-three").click(function(){
+$("#button-three").toggleClass("third")
+})
