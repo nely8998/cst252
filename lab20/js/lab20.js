cae264644c4765646d859da5859b3c18e10ff4d2
Nely1226 <nelyflores116@gmail.com>
2021-05-04 18:29:56 -0700

    Update lab20.js

--- lab20/js/lab20.js
+++ lab20/js/lab20.js
@@ -7,22 +7,13 @@
 
 
 // attach click action to button
-$('#activate').click(function(){
-    // get data via ajax from numbersapi
-    // Using the core $.ajax() method
-    $.ajax({
-        dataType: 'json',
-        // The URL for the request (ENDPOINT)
-        url: "https://xkcd.com/info.0.json",
-        // The data to send (will be converted to a query string)
-        // data: { id: 123},
-        // Whether this is a POST or GET request
-        type: "GET",
-        // The type of data we expect back
-        // dataType : "json",
-    })
-    // If the request succeeds
-    $.getJSON(url, data, function(data) {
-      // do stuff if we succeed
-      console.log(data);
-    });
+$.ajax({
+    url: "https://xkcd.com/info.0.json",
+    method: "GET",
+    success: function(data){
+        console.log(data);
+        $("h3").html(data.title);
+        $("#headshot").attr("src", data.img);
+        $("#education").html(data.alt);
+    }
+})
