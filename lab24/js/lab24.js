0c4e00591ddb78e0de1c630fd3ae80b83fbe2769
Nely1226 <nelyflores116@gmail.com>
2021-05-11 13:26:41 -0700

    n

--- lab24/js/lab24.js
+++ lab24/js/lab24.js
@@ -1,20 +1,5 @@
-//initialize throttleTimer as false
-let throttleTimer = false;
-
-const throttle = (callback, time) => {
-    //don't run the function while throttle timer is true
-    if (throttleTimer) return;
-
-    //first set throttle timer to true so the function doesn't run
-    throttleTimer = true;
-
-    setTimeout(() => {
-        //call the callback function in the setTimeout and set the throttle timer to false after the indicated time has passed
-        callback();
-        throttleTimer = false;
-    }, time);
-}
-
-window.addEventListener('scroll', () => {
-  throttle(handleScrollAnimation, 250);
-})
+AOS.init({
+        offset: 400, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 1000 // values from 0 to 3000, with step 50ms
+      });
